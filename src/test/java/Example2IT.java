d6cff07b985efb8d8bbe55a52ad3640877f1fe92
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-19 19:36:35 +0530

    first commit

---
+++ src/test/java/Example2IT.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Example2IT {
+
+	@Test
+	public void testFail() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(10, 10);
+	}
+
+}
