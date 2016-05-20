e2f6cf2115d2e2a42553789642cd08727fe4502b
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-20 15:46:05 +0530

    commit 5

---
+++ src/test/java/ITExample4Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class ITExample4Test {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(20, 30);
+	}
+
+}
